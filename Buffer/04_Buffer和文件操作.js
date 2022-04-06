73a370c90d7cedff8c32e71d4f96836e37f8352f
twitzz <2351061127@qq.com.com>
2022-04-06 21:10:41 +0800

    fix

--- Buffer/04_Buffer和文件操作.js
+++ Buffer/04_Buffer和文件操作.js
@@ -1,25 +1,28 @@
-const fs = require('fs')
+const fs = require("fs");
 // sharp库的导入
-const sharp = reuire('sharp')
+const sharp = require("sharp");
 // 读取文本文件
-fs.readFile('./foo.txt',{encoding:'utf8'},(err,data) => {
-    console.log(data)
-})
+fs.readFile("./foo.txt", { encoding: "utf8" }, (err, data) => {
+  console.log(data);
+});
 
 // 不传字符编码，读出来的是buffer
-fs.readFile('./foo.txt',(err,data) => {
-    console.log(data)
-    console.log(data.toString())
-})
+fs.readFile("./foo.txt", (err, data) => {
+  console.log(data);
+  console.log(data.toString());
+});
 
 // 读取图片文件
-fs.readFile('./bar.png',(err,data) => {
-    console.log(data)
-})
+fs.readFile("./bar.png", (err, data) => {
+  console.log(data);
+});
 
-sharp('./bar.png').resize(200,200).toFile('./baz.png')
-sharp('./bar.txt').resize(300,300).toBuffer().then(data => {
-    fs.writeFile('./bax.png',data,err => {
-        console.log(err)
-    } )
-})
+sharp("./bar.png").resize(200, 200).toFile("./baz.png");
+sharp("./bar.txt")
+  .resize(300, 300)
+  .toBuffer()
+  .then((data) => {
+    fs.writeFile("./bax.png", data, (err) => {
+      console.log(err);
+    });
+  });
